8ec8c6de50aee86e8cd6947a5bf5f68eba34e6ea
MichaelSH7 <ubuntu@ip-172-31-47-21.ec2.internal>
2023-11-03 23:13:30 +0000

    task4

--- query.py
+++ query.py
@@ -10,10 +10,10 @@ def main():
         print("q: Quit")
         print("list: List all movie names")
         print("sn: Search movies by name")
+        print("sc: Search movies by cast")
 
 
-
-        choice = input("enter your choice: ")
+        choice = input("enter you choice: ")
 
 
         if choice == "q":
@@ -24,6 +24,9 @@ def main():
         elif choice == "sn":
             keyword = input("type to search for movies by name: ")
             movies_by_name(keyword)
+        elif choice == "sc":
+            keyword = input("type to search for movies by cast: ")
+            movies_by_cast(keyword)
         else:
             print("Invalid option, try again.")
 
@@ -39,7 +42,13 @@ def movies_by_name(keyword):
     [print(movie) for movie in matching_movies]
 
 
-
+def movies_by_cast(keyword):
+    matching_movies = [movie_info['name'] for movie_info in movies._movies if any(keyword.lower() in actor.lower() for actor in movie_info['cast'])]
+    matching_actors = [actor for movie_info in movies._movies for actor in movie_info['cast'] if keyword.lower() in actor.lower()]
+    print("Matches:")
+    [print(movie) for movie in matching_movies]
+    print("Matching actors/actresses:")
+    [print(actor) for actor in matching_actors]
 
 
 if __name__ == "__main__":
