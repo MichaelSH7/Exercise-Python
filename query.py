0499ef328010bd72a2970e286b82d9cda66ef9b4
MichaelSH7 <ubuntu@ip-172-31-47-21.ec2.internal>
2023-11-03 23:05:05 +0000

    task2

--- query.py
+++ query.py
@@ -1,19 +1,36 @@
 from movies import Movies
 
+
 movies = Movies('./movies.txt')
+
 
 def main():
     while True:
         print("\nMenu:")
         print("q: Quit")
+        print("list: List all movie names")
+        choice = input("enter your choice: ")
 
-        choice = input("Enter your choice: ")
 
         if choice == "q":
-            print("Qutting")
+            print("Quiting...")
             break
+        elif choice == "list":
+            list_all_movie_names()
+       
         else:
-            print("Invalid option. Please try again.")
+            print("Invalid option, try again.")
+
+
+def list_all_movie_names():
+    print("Listing all movie names:")
+    [print(movie_info['name']) for movie_info in movies._movies]
+
+
+
 
 if __name__ == "__main__":
     main()
+
+
+
